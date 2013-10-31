21ca7bf8f0c4fd2bb9a632644f07e601e59d278a
Lars-Olof Moilanen <lars.olof.moilanen@gmail.com>
2013-10-31 09:38:20 +0100

    Fixing navbar links

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,4 +22,10 @@ var acnfagkveldApp = angular.module('acnfagkveldApp', [
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function ($rootScope, $location, $anchorScroll, $routeParams) {
+    $rootScope.$on('$routeChangeSuccess', function (newRoute, oldRoute) {
+      $location.hash($routeParams.scrollTo);
+      $anchorScroll();
+    });
   });
