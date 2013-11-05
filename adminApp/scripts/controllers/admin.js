20ce61af776f59b588a25244a15e1ea18afd4a42
Slomic, Ismar <ismar.slomic@accenture.com>
2013-11-05 01:25:38 +0100

    Created table with filtering and dummy buttons showing legal action for selected registrations.

--- adminApp/scripts/controllers/admin.js
+++ adminApp/scripts/controllers/admin.js
@@ -3,17 +3,79 @@ app.controller('AdminCtrl', function ($scope, registrationService) {
         $scope.registrationList = receivedRegistrationList;
     });
 
+    $scope.filterOptions = {
+        filterText: ''
+    };
+
+    $scope.canConfirm = false;
+    $scope.canUregister = false;
+    $scope.canCheckin = false;
+    $scope.canEmail = false;
+
+    $scope.updateLegalActions = function (registrations) {
+        var unique = {};
+        var distinct = [];
+        for (var i in registrations) {
+            if (typeof(unique[registrations[i].state]) == "undefined") {
+                distinct.push(registrations[i].state);
+            }
+            unique[registrations[i].state] = 0;
+        }
+
+        if (distinct.length != 1) {
+            $scope.canConfirm = false;
+            $scope.canUnregister = false;
+            $scope.canCheckIn = false;
+        }
+        else if (distinct[0] == "confirmed") {
+            $scope.canUnregister = true;
+            $scope.canCheckIn = true;
+        }
+        else if (distinct[0] == "registered") {
+            $scope.canUnregister = true;
+            $scope.canConfirm = true;
+        }
+
+        if (registrations.length > 0) {
+            $scope.canEmail = true;
+        }
+        else {
+            $scope.canEmail = false;
+        }
+    }
+
     $scope.gridOptions = {
         data: 'registrationList',
+        selectedItems: [],
         columnDefs: [
-            {field: 'state', displayName: 'Status'},
-            {field: 'person.name', displayName: 'Navn'},
-            {field: 'person.phone', displayName: 'Telefon'},
-            {field: 'person.email', displayName: 'Email'},
-            {field: 'created', displayName: 'Opprettet'},
-            {field: 'updated', displayName: 'Oppdatert'}
+            {field: 'state', displayName: 'Status', enableCellEdit: false},
+            {field: 'person.name', displayName: 'Navn', enableCellEdit: true},
+            {field: 'person.phone', displayName: 'Telefon', enableCellEdit: true},
+            {field: 'person.email', displayName: 'Email', enableCellEdit: true, resizable: true},
+            {field: 'created', displayName: 'Opprettet', cellFilter: 'date:\'dd.MMM.yy HH:mm\'', enableCellEdit: false},
+            {field: 'updated', displayName: 'Oppdatert', cellFilter: 'date:\'dd.MMM.yy HH:mm\'', enableCellEdit: false}
         ],
         showGroupPanel: true,
-        jqueryUIDraggable: true
+        jqueryUIDraggable: true,
+        filterOptions: $scope.filterOptions,
+        showFooter: true,
+        enableColumnResize: true,
+        sortInfo: {
+            fields: ['updated'], directions: ['desc']
+        },
+        i18n: 'nb',
+        afterSelectionChange: function () {
+            $scope.updateLegalActions($scope.gridOptions.selectedItems);
+        }
+    };
+
+    $scope.filterNephi = function () {
+        var filterText = 'name:Nephi';
+        if ($scope.filterOptions.filterText === '') {
+            $scope.filterOptions.filterText = filterText;
+        }
+        else if ($scope.filterOptions.filterText === filterText) {
+            $scope.filterOptions.filterText = '';
+        }
     };
 });
---
+++ adminApp/scripts/i18n/ng-grid-nb.js
@@ -0,0 +1,14 @@
+window.ngGrid.i18n['nb'] = {
+    ngAggregateLabel: 'artikler',
+    ngGroupPanelDescription: 'Grupper rader basert på kolonne(r) ved å trekke dem over hit.',
+    ngSearchPlaceHolder: 'Søk...',
+    ngMenuText: 'Velg kolonner:',
+    ngShowingItemsLabel: 'Viste rader:',
+    ngTotalItemsLabel: 'Rader totalt:',
+    ngSelectedItemsLabel: 'Valgte rader:',
+    ngPageSizeLabel: 'Paginering størrelse:',
+    ngPagerFirstTitle: 'Første side',
+    ngPagerNextTitle: 'Neste side',
+    ngPagerPrevTitle: 'Forrige side',
+    ngPagerLastTitle: 'Siste side'
+};
