d89e870c77ef6525515ea29cb7a9bc0b69b89ed0
Simen Sægrov <saegrov+github.com@gmail.com>
2013-11-24 19:02:27 +0100

    Add full html and css from acn-fagkveld frontend and linked angular controllers.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('acnEventsApp', [
-  'ngResource',
-  'ngRoute'
-])
+    'ngResource',
+    'ngRoute',
+    'ui.bootstrap',
+    'ui.bootstrap.modal'
+  ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'RegistrationCtrl'
+      })
+      .when('/registrations/:id/confirm', {
+        templateUrl: 'views/main.html',
+        controller: 'ConfirmRegistrationCtrl'
+      })
+      .when('/registrations/:id/unregister', {
+        templateUrl: 'views/main.html',
+        controller: 'DeleteRegistrationCtrl'
       })
       .otherwise({
         redirectTo: '/'
