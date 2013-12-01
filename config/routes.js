c499b0d21fec18dbb10ff5e2bf388b883f2b7ce5
Karl Gustav Røksund <kkgg84+git@gmail.com>
2013-12-01 16:23:33 +0100

    issue-23: Renamed lib/controllers/event.js -> lib/controllers/max_seats.js

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,19 @@
 module.exports = function (app) {
-  var registrationCtrl = require('../lib/controllers/registration.js');
-  var eventCtrl  = require('../lib/controllers/event.js');
-  var actionCtrl = require('../lib/controllers/actions.js');
+	var registrationCtrl = require('../lib/controllers/registration.js');
+	var masSeatsCtrl  = require('../lib/controllers/max_seats.js');
+	var actionCtrl = require('../lib/controllers/actions.js');
 
-  var regBaseUrl = '/api/event/registrations';
-  var eventBaseUrl = '/api/event';
+	var regBaseUrl = '/api/event/registrations';
+	var maxSeatsBaseUrl = '/api/event';
 
-  // Public routes
-  app.get(regBaseUrl + '/:id', registrationCtrl.readRegistration());
-  app.get(eventBaseUrl, eventCtrl.countConfirmedReg());
-  app.post(actionCtrl.register.route, registrationCtrl.register());
-  app.delete(actionCtrl.unregister.route, registrationCtrl.unregister());
-  app.post(actionCtrl.confirm.route, registrationCtrl.confirm());
+	// Public routes
+	app.get(regBaseUrl + '/:id', registrationCtrl.readRegistration());
+	app.get(maxSeatsBaseUrl, masSeatsCtrl.countConfirmedReg());
+	app.post(actionCtrl.register.route, registrationCtrl.register());
+	app.delete(actionCtrl.unregister.route, registrationCtrl.unregister());
+	app.post(actionCtrl.confirm.route, registrationCtrl.confirm());
 
-  // Private routes TODO: Authentication is removed. It's not private!
-  app.get(regBaseUrl,  registrationCtrl.readAllRegistrations());
-  app.post(actionCtrl.checkIn.route, registrationCtrl.checkIn());
+	// Private routes TODO: Authentication is removed. It's not private!
+	app.get(regBaseUrl,  registrationCtrl.readAllRegistrations());
+	app.post(actionCtrl.checkIn.route, registrationCtrl.checkIn());
 };
